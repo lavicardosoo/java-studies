0811f5aab1da1de6d2145ee396297f7c1ed266d3
lavicardosoo <technisy@gmail.com>
2025-07-14 23:24:10 -0300

    var

---
+++ variaveis/Var1.java
@@ -0,0 +1,15 @@
+package variaveis;
+
+public class Var1 {
+    public static void main(String args[]){
+        String nome = "Aphonsinha";
+        int idade = 17;
+        float altura = 1.96f;
+
+        System.out.println("\n=========================\n");
+        System.out.println("-Oi! O seu nome é:" + nome);
+        System.out.println("-Sua idade é:" + idade);
+        System.out.println("-E aparentemente, você tem " + altura + "m de altura!");
+        System.out.println("\n=========================");
+    }
+}
