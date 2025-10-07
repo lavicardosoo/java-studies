7621473c6f6d1675c9caeecf9d606270f3c13b09
Lavi <78282185+lavicardosoo@users.noreply.github.com>
2025-10-07 19:27:44 -0300

    Add files via upload

---
+++ exe-basic/jogo-da-veia/JogoDaVelha.java
@@ -0,0 +1,17 @@
+public class JogoDaVelha{
+public static void main(String[] args){
+System.out.println(
+"""
+
+--------------------------------
+
+⋆˚JOGO DA VELHA✩*⋆
+
+--------------------------------
+
+""");
+TabuleiroTrans tabuleiro = new TabuleiroTrans('O');	
+tabuleiro.rodarJogo();
+}
+}
+
